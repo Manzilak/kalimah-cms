69d0de5ce64298fe14af84ef45ebeb598997a14c
Abdullah Alghamdi <abdullah@manzilak.com>
2019-10-20 22:05:39 +0300

    Remove personal data

--- public/settings.js
+++ public/settings.js
@@ -1,11 +1,14 @@
 'use strict'
-
+// Use your own settings
+// Contact us to get a free testing account
+// info@manzilak.com
+// Use [xbort test account] in the title
 export const kmSettings = {
   XbUrl: 'https://xbuffer.net',
   XbPath: 'client',
-  XbUser: 'codingride',
+  XbUser: '...',
   XbProject: 'website',
-  XbDirectory: 'codingride',
+  XbDirectory: '...',
   XbProfiles: 'profiles',
-  XbAppID: 'bb42a7a45b0f183a8b3e8bca798b756b9ff140231bbe6c59df790fd4a9e95ea8:web'
+  XbAppID: '....:web'
 }
