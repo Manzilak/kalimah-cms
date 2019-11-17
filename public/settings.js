c8ef7016ed2c12b1e54bf57b8f10c7b0faf8fbf5
Abdullah Alghamdi <abdullah@manzilak.com>
2019-11-17 09:04:28 +0300

    Update settings.js

--- public/settings.js
+++ public/settings.js
@@ -3,9 +3,9 @@
 export const kmSettings = {
   XbUrl: 'https://xbuffer.net',
   XbPath: 'client',
-  XbUser: 'codingride',
-  XbProject: 'website',
-  XbDirectory: 'codingride',
-  XbProfiles: 'profiles',
-  XbAppID: 'bb42a7a45b0f183a8b3e8bca798b756b9ff140231bbe6c59df790fd4a9e95ea8:web'
+  XbUser: '...',
+  XbProject: '...',
+  XbDirectory: '...',
+  XbProfiles: '...',
+  XbAppID: '...:web'
 }
